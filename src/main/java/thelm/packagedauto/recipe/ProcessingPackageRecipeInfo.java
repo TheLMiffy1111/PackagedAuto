26a16fca5e369789454b202e3686296aa6c5ea5c
unknown <TheLMiffy1111@users.noreply.github.com>
2022-04-12 19:42:39 +0800

    Fixed encoder processing recipe load loading extra outputs

--- src/main/java/thelm/packagedauto/recipe/ProcessingPackageRecipeInfo.java
+++ src/main/java/thelm/packagedauto/recipe/ProcessingPackageRecipeInfo.java
@@ -87,9 +87,6 @@ public class ProcessingPackageRecipeInfo implements IPackageRecipeInfo {
 		for(int i = 0; i < output.size(); ++i) {
 			map.put(i+81, output.get(i));
 		}
-		for(int i = 0; i < output.size() && i < 3; ++i) {
-			map.put(i*3+81+1, output.get(i));
-		}
 		return map;
 	}
 
