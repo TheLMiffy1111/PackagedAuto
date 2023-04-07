7c53e3197285663d293ad1c31e30e15277ba388f
unknown <TheLMiffy1111@users.noreply.github.com>
2023-04-07 17:08:04 +0800

    Fix crafter comparator output not checking output slot

--- src/main/java/thelm/packagedauto/PackagedAuto.java
+++ src/main/java/thelm/packagedauto/PackagedAuto.java
@@ -24,7 +24,7 @@ public class PackagedAuto {
 
 	public static final String MOD_ID = "packagedauto";
 	public static final String NAME = "PackagedAuto";
-	public static final String VERSION = "1.12.2-1.0.8.30";
+	public static final String VERSION = "1.12.2-1.0.8.31";
 	public static final String DEPENDENCIES = "";
 	public static final String GUI_FACTORY = "thelm.packagedauto.client.gui.GuiPackagedAutoConfigFactory";
 	public static final CreativeTabs CREATIVE_TAB = new CreativeTabs("packagedauto") {
--- src/main/java/thelm/packagedauto/tile/TileCrafter.java
+++ src/main/java/thelm/packagedauto/tile/TileCrafter.java
@@ -197,7 +197,7 @@ public class TileCrafter extends TileBase implements ITickable, IPackageCrafting
 		if(isWorking) {
 			return 1;
 		}
-		if(!inventory.stacks.subList(0, 9).stream().allMatch(ItemStack::isEmpty)) {
+		if(!inventory.stacks.subList(0, 10).stream().allMatch(ItemStack::isEmpty)) {
 			return 15;
 		}
 		return 0;
