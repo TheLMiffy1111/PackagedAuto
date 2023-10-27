13b2586d92591ea5c49c9947a29470fbfe3498f7
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2023-10-28 00:11:45 +0800

    Made machines still eject if a previous slot was not ejected

--- src/main/java/thelm/packagedauto/block/entity/CrafterBlockEntity.java
+++ src/main/java/thelm/packagedauto/block/entity/CrafterBlockEntity.java
@@ -16,6 +16,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemHandlerHelper;
 import thelm.packagedauto.api.IPackageCraftingMachine;
 import thelm.packagedauto.api.IPackageRecipeInfo;
 import thelm.packagedauto.block.CrafterBlock;
@@ -131,26 +132,13 @@ public class CrafterBlockEntity extends BaseBlockEntity implements IPackageCraft
 			BlockEntity blockEntity = level.getBlockEntity(worldPosition.relative(direction));
 			if(blockEntity != null && !(blockEntity instanceof UnpackagerBlockEntity) && blockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER, direction.getOpposite()).isPresent()) {
 				IItemHandler itemHandler = blockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER, direction.getOpposite()).resolve().get();
-				boolean flag = true;
 				for(int i = 9; i >= endIndex; --i) {
 					ItemStack stack = this.itemHandler.getStackInSlot(i);
 					if(stack.isEmpty()) {
 						continue;
 					}
-					for(int slot = 0; slot < itemHandler.getSlots(); ++slot) {
-						ItemStack stackRem = itemHandler.insertItem(slot, stack, false);
-						if(stackRem.getCount() < stack.getCount()) {
-							stack = stackRem;
-							flag = false;
-						}
-						if(stack.isEmpty()) {
-							break;
-						}
-					}
-					this.itemHandler.setStackInSlot(i, stack);
-					if(flag) {
-						break;
-					}
+					ItemStack stackRem = ItemHandlerHelper.insertItem(itemHandler, stack, false);
+					this.itemHandler.setStackInSlot(i, stackRem);
 				}
 			}
 		}
--- src/main/java/thelm/packagedauto/block/entity/PackagerExtensionBlockEntity.java
+++ src/main/java/thelm/packagedauto/block/entity/PackagerExtensionBlockEntity.java
@@ -21,6 +21,7 @@ import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.RecipeMatcher;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.ItemStackHandler;
 import thelm.packagedauto.api.IPackageItem;
 import thelm.packagedauto.api.IPackagePattern;
@@ -290,16 +291,8 @@ public class PackagerExtensionBlockEntity extends BaseBlockEntity {
 			if(level.getBlockEntity(worldPosition.relative(direction)) instanceof UnpackagerBlockEntity unpackager) {
 				ItemStack stack = itemHandler.getStackInSlot(9);
 				if(!stack.isEmpty()) {
-					for(int slot = 0; slot < 9; ++slot) {
-						ItemStack stackRem = unpackager.itemHandler.insertItem(slot, stack, false);
-						if(stackRem.getCount() < stack.getCount()) {
-							stack = stackRem;
-						}
-						if(stack.isEmpty()) {
-							break;
-						}
-					}
-					itemHandler.setStackInSlot(9, stack);
+					ItemStack stackRem = ItemHandlerHelper.insertItem(unpackager.itemHandler, stack, false);
+					itemHandler.setStackInSlot(9, stackRem);
 				}
 			}
 		}
