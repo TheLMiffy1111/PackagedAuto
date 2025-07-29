055da308803c2ea70fddfc1c39651f920e02d6c8
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-07-29 16:32:23 +0800

    Change how encoder slots are reset

--- src/main/java/thelm/packagedauto/menu/BaseMenu.java
+++ src/main/java/thelm/packagedauto/menu/BaseMenu.java
@@ -38,7 +38,7 @@ public class BaseMenu<T extends BaseBlockEntity> extends AbstractContainerMenu {
 		return 84;
 	}
 
-	public void setupPlayerInventory() {
+	protected void setupPlayerInventory() {
 		int xOffset = getPlayerInvX();
 		int yOffset = getPlayerInvY();
 		for(int i = 0; i < 3; i++) {
