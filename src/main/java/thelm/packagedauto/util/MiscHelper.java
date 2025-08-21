a071a176007df228e03269264eca1de31be18a55
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-08-22 00:27:11 +0800

    Update MiscHelper.java

--- src/main/java/thelm/packagedauto/util/MiscHelper.java
+++ src/main/java/thelm/packagedauto/util/MiscHelper.java
@@ -121,7 +121,7 @@ public class MiscHelper implements IMiscHelper {
 			while(count > 0) {
 				ItemStack toAdd = new ItemStack(item, 1);
 				toAdd.setTag(nbt);
-				int limit = ignoreStackSize ? 1000000000 : item.getMaxStackSize(toAdd);
+				int limit = ignoreStackSize ? 1000000000 : Math.min(item.getMaxStackSize(toAdd), 1000000000);
 				toAdd.setCount((int)Math.min(count, limit));
 				list.add(toAdd);
 				count -= limit;
